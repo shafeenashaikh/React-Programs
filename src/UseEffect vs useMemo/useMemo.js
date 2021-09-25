813ac78edbc7c0a1df3b784034622182a6006bd7
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-25 11:58:24 +0530

    rendering with useMemoHook

--- src/UseEffect vs useMemo/useMemo.js
+++ src/UseEffect vs useMemo/useMemo.js
@@ -3,15 +3,16 @@ import React, { useMemo,useState } from "react";
 function UseMemo(){
 
     const[count,setCount]=useState(0)
+    const[name,setName]=useState(0)
    
     const noRender = useMemo(()=>{
         return(
             <div>
-            <h1 style={{color:'red'}}>No render again {count}</h1>
+            <h1 style={{color:'red'}}>No render again {count} -- {name}</h1>
             <h1 style={{color:'red'}}>UseEffect vs useMemo</h1>
             </div>
         )
-    },[])
+    },[name])
     
     return(
         <div>
@@ -21,6 +22,7 @@ function UseMemo(){
             <h1>UseEffect vs useMemo</h1>
 
             <button onClick={()=>setCount(count+1)}>Click</button>
+            <button onClick={()=>setName('name')}>Click</button>
         </div>
     )
 }
